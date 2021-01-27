3fdf5dcdea4f33a164b0e38bf17f3692417edb00
Jagadish Patil <jagadishpatil231@gmail.com>
2021-01-27 19:43:35 +0000

    fix: validating input for prime number birthday

--- index.js
+++ index.js
@@ -11,26 +11,43 @@ console.log(chalk.green("Welcome " + username.toUpperCase()));
 
 console.log(chalk.yellow("\tLets see, whether your Birthday is prime or not?\n"));
 
-var birthDate = readlineSync.question("Enter your Date of Birth (in DD format) ");
+var dob = prompt("Enter your Date of Birth? (in DD/MM format)\n");
 
-console.log();
-if(isNaN(birthDate)){
-  console.log(chalk.bgCyan("Invalid Input!, you did not mention your Date of Birth"));
-} 
-else{
-prime(birthDate);
+var dd = "";
+var mm = "";
+for(var i = 0; i < dob.length; i++){
+  // for date
+  dd = dob[0] + dob[1];
+  // for month
+  mm = dob[3] + dob[4];  
+}
+
+format = dd + "/" + mm;
+dd = parseInt(dd);
+mm = parseInt(mm);
+
+if(mm === 2 && dd >= 30 ){
+  console.log(chalk.bgCyan("Invalid Input!, Its a February month"));
+}
+else if(dd > 31){
+  console.log(chalk.bgCyan("Invalid Input!, Date is more than 31"));  
+}
+else if(mm > 12){
+  console.log(chalk.bgCyan("Invalid Input!, Month is more than 12"));  
+}
+else if(dd < 1){
+  console.log(chalk.bgCyan("Invalid Input!, Date is less than 1"));
+}
+else if(dob != format){
+  console.log(chalk.bgCyan("Invalid Input!"));
+}
+else{  
+  prime(dd);
 }
 
 //processing
 function prime(birthDate){
   var flag = true;
-  if(birthDate > 31){
-    console.log(chalk.bgCyan(birthDate + " is not a valid Birth Date."));
-  } 
-  else if(birthDate <= 0){
-    console.log(chalk.bgCyan(birthDate + " is not a valid Birth Date."));
-  }
-  else{
   if(birthDate === 1){
     console.log(chalk.bgCyan(birthDate + " is neither prime nor composite."));
   }
@@ -49,9 +66,8 @@ function prime(birthDate){
       console.log("##########");
     }
     else{
-      console.log(chalk.bgCyan(birthDate + " is not a prime number."));
+      console.log(chalk.bgYellow(birthDate + " is not a prime number."));
     }
-  }
   }
 }
 
